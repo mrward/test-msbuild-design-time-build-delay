1b3dff5b725e4e4aa365b467dc391d0625113455
Matt Ward <matt.ward@microsoft.com>
2021-08-17 09:57:07 +0100

    Add delay msbuild task

---
+++ MSBuildDelayDesignTimeBuildTask/DelayDesignTimeBuild.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Build.Utilities;
+using System.Threading;
+
+namespace MSBuildDelayDesignTimeBuildTask
+{
+	public class DelayDesignTimeBuild : Task
+	{
+		public int Delay { get; set; } = 30000;
+
+		public override bool Execute ()
+		{
+			Log.LogMessage ("Sleeping for {0} ms", Delay);
+
+			Thread.Sleep (Delay);
+
+			Log.LogMessage ("Finished sleep");
+
+			return true;
+		}
+	}
+}
+
